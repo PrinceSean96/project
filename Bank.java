f0665157a7657a7f59d35010d2d0129f9ec2df7b
Sean Mayers <31990140+PrinceSean96@users.noreply.github.com>
2021-05-26 01:27:47 -0400

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,49 @@
+import java.util.scanner;
+
+public class Bank {
+
+	// variables 
+	int balance;
+	// line above basically initializes the users balance
+	String customerid; 
+	// id that is unique to the user
+	String customername;
+	// displays the customers name
+	int last_transaction;
+	// shows previous transactions
+	
+	// constructor
+	
+	Bank(String cname, String cid){
+		customername = cname;
+		customerid = cid;
+		
+	}
+	
+	void withdraw() {
+		// this function calculates and shows what money has been withdrew
+		// return balance
+	}
+	
+	void getTransactions() {
+		// shows transactions done in the account
+		// return balance
+	}
+	
+	void options() {
+		// user interface
+		Scanner scanner = new Scanner(System.in); 
+		
+		// what is your name
+		// password
+		// what do you want to do
+		// withdraw
+		// 
+		view information
+	} 
+	
+	
+	
+	
+	
+}
